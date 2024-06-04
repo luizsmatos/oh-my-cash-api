390e2a6a80c605e8bec31e2b5f4151f68de15011
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-04 11:18:14 -0300

    feat(app.module.ts): add EnvModule to imports in app module for better organization and separation of concerns

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 
+import { EnvModule } from './config/env/env.module'
+
 @Module({
-  imports: [],
+  imports: [EnvModule],
   controllers: [],
   providers: [],
 })
